2aa5ada74bb97eb0e0bf261b3a76bd8c4326d369
ms.ksenialekseeva <ms.ksenialekseeva@mail.ru>
2022-04-03 20:55:37 +0300

    added lesson-5 homework

---
+++ lesson-5/js/main-vue.js
@@ -0,0 +1,79 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        products: [],
+        filtered: [],
+        cart: [],
+        imgCatalog: 'https://www.ldoceonline.com/media/english/illustration/kitten.jpg',
+        userSearch: '',
+        show: false
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        // метод выполняет поиск товаров, вводимых в поле поиска
+        filter(){
+            const regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.product_name));
+           },
+
+        // метод добавляет товар в корзину
+        addProduct(cartItem){
+            this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                if(data.result === 1) {
+                    let find = this.cart.find (el => el.id_product === cartItem.id_product);
+                    if (find) {
+                        find.quantity++;
+                    } else {
+                        const prod = Object.assign({quantity: 1}, cartItem);
+                        this.cart.push(prod);
+                    }
+                }
+            })
+        },
+
+        // метод удаляет товар из корзины
+        remove(cartItem) {
+            this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                if(cartItem.quantity > 1) {
+                    cartItem.quantity--;
+                } else {
+                    this.cart.splice(this.cart.indexOf(cartItem), 1)
+                }
+            })
+        }
+    },
+    mounted(){
+       this.getJson(`${API + this.catalogUrl}`)
+           .then(data => {
+               for(let el of data){
+                   this.products.push(el);
+                   this.filtered.push(el);
+               }
+           });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+            this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for(let el of data.contents){
+                    this.cart.push(el);
+                }
+            })
+    }
+})
